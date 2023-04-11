e3c6cde2ef823cb3a6977cd10b8120ae2274a53d
lokesh783 <77091485+lokesh783@users.noreply.github.com>
2023-04-11 21:41:13 +0530

    Update check cycle in dg.cpp

--- Graph/check cycle in dg.cpp
+++ Graph/check cycle in dg.cpp
@@ -1,4 +1,23 @@
-        for(auto vec: p) {
+
+// USING DFS
+bool detectCycle(int src ,vector<int>&vis,vector<int>&crvisit,vector<int> adj[]){
+        vis[src]=1;
+        crvisit[src]=1;
+        for(auto it  : adj[src]){
+            if(!vis[it]){
+                if(detectCycle(it , vis , crvisit , adj)) return true ;
+            }
+            else if(vis[it]  && crvisit[it]){
+                return true ;
+            }
+        }
+        crvisit[src]=0;
+        return false ;
+    }
+
+
+// USING KAHN'S ALGO
+for(auto vec: p) {
             g[vec[1]].push_back(vec[0]);
             inDeg[vec[0]]++;
         }
